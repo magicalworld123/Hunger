4596c936b50612c483e5edbbff0c89da17c02267
ASHUTOSH KUMAR <51821878+magicalworld123@users.noreply.github.com>
2020-09-27 13:09:01 +0530

    Init commit

---
+++ Hungerspot.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+
+<html lang="">
+
+<head>
+<title>Hunger Free India</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear">
+    <div class="fl_left"> 
+      <!-- ################################################################################################ -->
+      <ul class="nospace">
+        <li><i class="fas fa-phone rgtspace-5"></i>7012259789</li>
+        <li><i class="far fa-envelope rgtspace-5"></i>ashu@gmail.com</li>
+      </ul>
+      <!-- ################################################################################################ -->
+    </div>
+    <div class="fl_right"> 
+      <!-- ################################################################################################ -->
+      <ul class="nospace">
+       
+        <li id="searchform">
+          <div>
+            <form action="#" method="post">
+              <fieldset>
+                <legend>Quick Search:</legend>
+                <input type="text" placeholder="Enter search term&hellip;">
+                <button type="submit"><i class="fas fa-search"></i></button>
+              </fieldset>
+            </form>
+          </div>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</div>
+
+<div class="wrapper row1">
+  <header id="header" class="hoc clear">
+    <div id="logo" class="fl_left"> 
+      
+      <h1><a href="index.html">Hunger Free India</a></h1>
+     
+    </div>
+    <nav id="mainav" class="fl_right"> 
+      
+      <ul class="clear">
+        <li ><a href="index.php">Home</a></li>
+        <li><a href="Volunteer.php">Volunteer</a></li>
+        <li><a href="Donate.php">Donate Food</a></li>
+        <li class="active"><a href="Hungerspot.php">Hunger Spots</a></li>
+         <li><a href="About.php">About Us</a></li>
+        <li><a href="login.php"> Admin Login</a></li>
+        
+      </ul>
+     
+    </nav>
+  </header>
+</div>
+
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">  
+        <title>Hunger Spots - NoFoodWaste</title>
+        <link rel="shortcut icon" href="https://www.nofoodwaste.in/wp-content/uploads/2016/04/32x32.png">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js'></script>
+        <!-- <script type='text/javascript' src='jquery-ui-1.8.14.custom.min.js'></script>-->
+        
+       
+    
+        <div class="container">
+            <div class="page-header">
+                
+                <h3><u>Hunger spots:</u></h3>
+                <p class="text-justify">Hunger spots are the areas where the needy and poor people dwell with hunger. We have mapped a few and we need your volunteering in identifying more!</p>
+                <p>Download our <b>“No Food Waste”</b> Android Mobile Application. </p>
+                <p>Take a picture of such hunger spot nearby to your locality. </p>
+                <p>Add in the hunger map with Geotag & approximate number of people. That’s it!!</p>
+                <p>Now, you have helped us in feeding the real needy!!</p>
+            </div>
+          </br>
+          </br>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfyWD4pFdyH9IRGZgpW9Mg-d-xYNnfLPmoc1_sc38A2JPCPdw/viewform?embedded=true" width="640" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </br>
+      </br>
+           
+            </div>
+        </div>
+    
+    </body>
+</html>
+
+<div class="wrapper row2">
+  <section id="latest" class="hoc container clear"> 
+    
+    <div class="sectiontitle">
+      <h6 class="heading">The Changes made due to Donating food</h6>
+      <p>If everyone is going to help a huge difference can be  made in our Society.  </p>
+    </div>
+    <ul class="nospace group">
+      <li class="one_third">
+        <figure><a class="imgover" href="#"><img src="images/child.jpg" alt=""></a>
+          <figcaption>Feeding Children</figcaption>
+        </figure>
+      </li>
+      <li class="one_third">
+        <figure><a class="imgover" href="#"><img src="images/waste food.jpg" alt=""></a>
+          <figcaption>Stop wasting food</figcaption>
+        </figure>
+      </li>
+      <li class="one_third">
+        <figure><a class="imgover" href="#"><img src="images/a.jpg" alt=""></a>
+          <figcaption>Stale food used as Fertilizers</figcaption>
+        </figure>
+      </li>
+    </ul>
+   
+    
+  </section>
+</div>
+
+<div class="wrapper row3">
+  <section id="cta" class="hoc container clear"> 
+   
+  </section>
+</div>
+
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+    
+    <div>
+      <h6 class="heading">About us</h6>
+      <p>About Us
+No Food Waste is a mission to end food waste and hunger to make the “World Hunger Free”. We recover surplus food from weddings, parties and functions and donate it to needy and hungry people.
+.</p>
+      <p class="btmspace-30">
+Dedicated Food Recovery Units including Helpline Number, Food Recovery Vehicles and Volunteers are involved for effective process execution[<a href="#"><i class="fas fa-arrow-right"></i></a>]</p>
+      
+    </div>
+    <div >
+     
+        <li>
+          
+        </li>
+      </ul>
+    </div>
+     </div>
+    
+    </div>
+    
+  </footer>
+</div>
+
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear">
+    <p class="fl_left">Copyright &copy; 2019 - All Rights Reserved <a href="#"></a></p>
+    <p class="fl_right"></a></p>
+   
+  </div>
+</div>
+
+<a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,49 @@
+<?php
+include('session.php');
+if(!isset($_SESSION['login_user'])){
+header("location: index.php"); // Redirecting To Home Page
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Your Home Page</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="profile">
+<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<b id="logout"><a href="logout.php">Log Out</a></b>
+</div>
+</body>
+</html>
